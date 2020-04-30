e9278f317375fac6ba009c90ca53c4fc290664ca
Arjun Panicker <arjunpanicker95@gmail.com>
2020-04-30 20:18:55 +0530

    Stable app

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { IonSlides, ToastController } from '@ionic/angular';
+import { IBoyRoutineList } from '../models/boy_routine.model';
+import { TextToSpeechService } from '../_services/utility_Services/text-to-speech.service';
 
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
 })
-export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
+export class ExploreContainerComponent implements OnChanges {
+  @Input('routineList') routineList: IBoyRoutineList;
 
-  constructor() { }
+  @ViewChild('slides', {static: true}) slides: IonSlides;
 
-  ngOnInit() {}
+  constructor(
+    private _ttsService: TextToSpeechService,
+    private _toastController: ToastController,
+    public cdRef: ChangeDetectorRef,
+  ) { }
 
+  public ngOnChanges(changes: SimpleChanges) {
+    if (this.routineList && this.routineList.routine && this.routineList.routine.length > 0) {
+      this.cdRef.detectChanges();
+    }
+  }
+
+  public previousSlide() {
+    this.slides.slidePrev();
+  }
+
+  public nextSlide() {
+    this.slides.slideNext();
+  }
+
+  public pronounce(text: string) {
+    this._ttsService.getSpeach(text).then(() => {
+      console.log('done!!');
+    }).catch(async (error) => {
+      const toast = await this._toastController.create({
+        message: 'Unable to convert into speech',
+        duration: 4000
+      });
+      toast.present();
+    })
+  }
 }
--- src/app/explore-container/explore-container.module.ts
+++ src/app/explore-container/explore-container.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { ServicesModule } from '../_services/services.module';
 import { ExploreContainerComponent } from './explore-container.component';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule],
+  imports: [ CommonModule, FormsModule, IonicModule, ServicesModule],
   declarations: [ExploreContainerComponent],
   exports: [ExploreContainerComponent]
 })
