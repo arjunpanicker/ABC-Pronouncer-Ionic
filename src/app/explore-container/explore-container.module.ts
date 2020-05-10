541c9bd7de7046be57e06482ff7c66cdd27abeba
Arjun Panicker <arjunpanicker95@gmail.com>
2020-05-10 12:33:33 +0530

    Removed unwanted directive

--- src/app/explore-container/explore-container.module.ts
+++ src/app/explore-container/explore-container.module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { ThrottleClickDirective } from '../_services/directives/debounce-click.directive';
 import { ExploreContainerComponent } from './explore-container.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule],
-  declarations: [ExploreContainerComponent, ThrottleClickDirective],
+  declarations: [ExploreContainerComponent],
   exports: [ExploreContainerComponent]
 })
 export class ExploreContainerComponentModule { }
